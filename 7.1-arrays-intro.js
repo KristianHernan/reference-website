273fddd5359ffe87c87b6e1509bd373e1e041a4b
KristianHernan <125923862+KristianHernan@users.noreply.github.com>
2023-03-07 17:30:46 +0000

    finished 7.1

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,21 @@
+function example() {
+  var topStudents = ["Christian", "Kristian", "Shakka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+}
+
+function problem1() {
+    var travelList = ["Ireland", "Denmark", "Germany", "Brazil", "Japan"];
+    var place = travelList[1];
+    console.log(place);
+}
+
+
+function problem2(){
+    var numList = [2, 3, 5, 7, 11];
+    console.log(numList[0]);
+    console.log(numList[2]);
+    console.log(numList[4]);
+}
